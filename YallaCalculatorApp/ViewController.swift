e12692ef2c1259eccaf3c3eaa060d5f9c8eb9e7d
RaviCYalla <ravichandrayalla222@gmail.com>
2023-02-12 20:19:47 -0600

    library

--- YallaCalculatorApp/ViewController.swift
+++ YallaCalculatorApp/ViewController.swift
@@ -6,14 +6,213 @@
 //
 
 import UIKit
+import MathExpression
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mathExpressionLBL: UILabel!
+    
+    
+    var currentexpression = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        mathExpressionLBL.text = ""
+        mathExpressionLBL.numberOfLines = 0
+        
     }
 
+    @IBAction func tappedChar(_ sender: UIButton) {
+        guard let character = sender.titleLabel?.text else {
+            return}
+            currentexpression += character
+            mathExpressionLBL.text = currentexpression
+        }
+        
+        
+//        guard let buttonTitle = sender.titleLabel, let buttonText = buttonTitle.text else{
+//            return
+//        }
+//        guard let expression = self.mathExpressionLBL.text else {
+//            return
+//        }
+        
+//        if let char = sender.titleLabel?.text
+//        {
+//            mathExpressionLBL.text! += char
+//        }
+        
+       // mathExpressionLBL.text! += sender.titleLabel!.text!
+        
+      //  expression += sender.titleLabel!.text!
+      //  mathExpressionLBL.text = expression
+        
+        
+    
+    
+//    if buttonText == "="{
+//        let value = self.evaluate(exp: expression + "+0")
+//       // let value = self.eva(exp: expression + "+0")
+//        self.mathExpressionLBL.text = self.isI
+//    }
+        
+        
+        
+        
+        
+        
+    
+        
+        
+    @IBAction func factorial(_ sender: UIButton) {
+        if let currentText = mathExpressionLBL.text, let currentNumber = Double(currentText){
+            let result = factorial(of: currentNumber)
+            mathExpressionLBL.text = "\(result)"
+        }
+    }
+    
+    func factorial(of number: Double) -> Double{
+        if number == 0 {
+            return 1
+        }
+        else{
+            return number * factorial(of : number - 1)
+        }
+    }
+      
+       // mathExpressionLBL.text = String(result)
+//        guard let expression = Double(currentexpression)
+//        else{
+//            return
+//        }
+//        let result =  factorial(expression)
+//        mathExpressionLBL.text = String(result)
+    
+   
+    
+        
+        
+        
+    
+    @IBAction func result(_ sender: UIButton) {
+        let expressionEvaluator = NSExpression(format: currentexpression)
+        if let result = expressionEvaluator.expressionValue(with: nil, context: nil) as? Double {
+            mathExpressionLBL.text = "\(result)"
+            
+        }
+        else{
+            mathExpressionLBL.text = "Error"
+        }
+        
+    }
+    
+    
+    @IBAction func clearExpression(_ sender: UIButton) {
+        mathExpressionLBL.text = ""
+    }
+    
+    @IBAction func backspace(_ sender: UIButton) {
+        
+        
+        if let currenttext = mathExpressionLBL.text{
+            mathExpressionLBL.text = String(currenttext.dropLast())
+        }
+     //   currentexpression.removeLast()
+       // mathExpressionLBL.text = currentexpression
+    }
+    
+    @IBAction func flipSign(_ sender: UIButton) {
+        
+        guard let expression = Double(currentexpression) else {
+            return
+        }
+        let result = -expression
+        mathExpressionLBL.text = String(result)
+    }
+    
+    @IBAction func percent(_ sender: UIButton) {
+        
+        guard let expression = Double(currentexpression) else
+        {
+            return
+        }
+        let result = expression/100
+        mathExpressionLBL.text = String(result)
+    }
+    
+    
+    
+    @IBAction func naturalLog(_ sender: UIButton) {
+        guard let expression = Double(currentexpression) else
+        {
+            return
+        }
+        let result = expression/100
+        mathExpressionLBL.text = String(result)
+    }
+    
+    
+    @IBAction func tenpow(_ sender: UIButton) {
+        guard let expression = Double(currentexpression)
+        else{
+            return
+        }
+        let result = pow(10, expression)
+        mathExpressionLBL.text = String(result)
+    }
+    
+    
+    @IBAction func calcSin(_ sender: UIButton) {
+        guard let expression = Double(currentexpression) else {
+            return
+        }
+        let result = sin(expression)
+        mathExpressionLBL.text = String(result)
+    }
+    
+    
+    @IBAction func calcCos(_ sender: UIButton) {
+        
+        guard let expression = Double(currentexpression) else {
+            return
+        }
+        let result = cos(expression)
+        mathExpressionLBL.text = String(result)
+    }
+    
+    @IBAction func calcTan(_ sender: UIButton) {
+        guard let expression = Double(currentexpression) else {
+            return
+        }
+        let result = tan(expression)
+        mathExpressionLBL.text = String(result)
+        
+    }
+    
+    
+    @IBAction func inverse(_ sender: UIButton) {
+        guard let expression = Double(currentexpression) else{
+            return
+        }
+        let result = 1/expression
+        mathExpressionLBL.text = String(result)
+        
+    }
+    
+    
+    }
 
-}
+//    private func evaluate(exp: String) -> Double {
+//        do{
+//            let expression = try MathExpression(exp)
+//            return expression.evaluate()
+//        }
+//        catch{
+//            print("Invalid expression")
+//        }
+//
+//    }
+    
+
 
